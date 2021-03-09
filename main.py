2c4f900c81a445ab04c66ced0be4a8a790aeb62e
Anders Petershagen Åsbø <aanders35@gmail.com>
2021-03-09 13:55:13 +0000

    added encouragements

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import discord
 import os
+import requests
+import json
+import random
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there!",
+  "You are a great person!"
+]
+
+def get_quote():
+  response = requests.get("http://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
 
 @client.event
 async def on_ready():
   print(f"We have logged in as {client.user}")
 
 @client.event
-async def on_message():
+async def on_message(message):
   if message.author == client.user:
     return
+
+  msg = message.content
   
-  if message.content.startswith("$hello"):
-    await message.channel.send("Hello!")
+  if msg.startswith("$inspire"):
+    quote = get_quote()
+    await message.channel.send(quote)
+  
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv("TOKEN"))
